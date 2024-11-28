20ed8860ebe53ea9573244a2b1ebc29583b7059e
Fabregasz <fabricioflaviofranca@gmail.com>
2024-11-28 10:13:44 -0300

    Update Skateboard.java

---
+++ com/example/transport/Skateboard.java
@@ -0,0 +1,12 @@
+public class Skateboard implements Transport {
+
+    
+    public void move() {
+        System.out.println("Skate se movendo...");
+    }
+
+   
+    public void stop() {
+        System.out.println("Skate parando...");
+    }
+}
