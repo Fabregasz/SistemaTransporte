4fc1e04817ffca5ed26f7621e8f5e497534406ed
Fabregasz <fabricioflaviofranca@gmail.com>
2024-11-28 10:12:18 -0300

    Update ElectricScooter.java

---
+++ com/example/transport/ElectricScooter.java
@@ -0,0 +1,12 @@
+public class ElectricScooter implements Transport {
+
+   
+    public void move() {
+        System.out.println("Scooter elétrica se movendo...");
+    }
+
+  
+    public void stop() {
+        System.out.println("Scooter elétrica parando...");
+    }
+}
