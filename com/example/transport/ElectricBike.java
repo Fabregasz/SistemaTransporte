e698f35c3c81862bfd9c842c7d3d936d3b7f6b3a
Fabregasz <fabricioflaviofranca@gmail.com>
2024-11-28 10:12:45 -0300

    Update ElectricBike.java

---
+++ com/example/transport/ElectricBike.java
@@ -0,0 +1,12 @@
+public class ElectricBike implements Transport {
+
+    
+    public void move() {
+        System.out.println("Bicicleta elétrica se movendo...");
+    }
+
+  
+    public void stop() {
+        System.out.println("Bicicleta elétrica parando...");
+    }
+}
