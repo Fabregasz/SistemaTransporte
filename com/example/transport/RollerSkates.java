9f7716152e6781bf0ac291b5570811904cd8a849
Fabregasz <fabricioflaviofranca@gmail.com>
2024-11-28 10:13:18 -0300

    Update RollerSkates.java

---
+++ com/example/transport/RollerSkates.java
@@ -0,0 +1,12 @@
+public class RollerSkates implements Transport {
+
+   
+    public void move() {
+        System.out.println("Patins se movendo...");
+    }
+
+   
+    public void stop() {
+        System.out.println("Patins parando...");
+    }
+}
